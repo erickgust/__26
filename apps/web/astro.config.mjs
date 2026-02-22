74286b6aa73d2401985dabb09178cca798357fa3
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2026-02-22 16:15:37 -0300

    add Google Fonts integration and update layout styles

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,11 +1,20 @@
 import node from "@astrojs/node";
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, envField } from "astro/config";
+import { defineConfig, envField, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Geist Mono",
+        cssVariable: "--font-geist-mono",
+      },
+    ],
+  },
   adapter: node({ mode: "standalone" }),
   env: {
     schema: {
