6b9db3724d1e7982aab1d3e96178e38ae4c85207
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2026-02-26 01:02:32 -0300

    feat: add Experience components for improved structure and reusability

---
+++ apps/web/src/components/Experience/index.ts
@@ -0,0 +1,6 @@
+export { default as Root } from "./Root.astro";
+export { default as Header } from "./Header.astro";
+export { default as Info } from "./Info.astro";
+export { default as Period } from "./Period.astro";
+export { default as Body } from "./Body.astro";
+export { default as Description } from "./Description.astro";
