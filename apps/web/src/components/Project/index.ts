ad2bf1dd94be048b847e22ea971125ac63c4993f
Erick Gustavo <83595333+erickgust@users.noreply.github.com>
2026-02-26 00:49:38 -0300

    feat: implement project components for improved structure and reusability

---
+++ apps/web/src/components/Project/index.ts
@@ -0,0 +1,6 @@
+export { default as Root } from "./Root.astro";
+export { default as Header } from "./Header.astro";
+export { default as Body } from "./Body.astro";
+export { default as Title } from "./Title.astro";
+export { default as Description } from "./Description.astro";
+export { default as Preview } from "./Preview.astro";
